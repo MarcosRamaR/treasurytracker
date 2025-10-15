4e4329ac26a58bb5ff49ebf07fe7f3c8c398e476
MarcosRamaR <marcos.rama.1994@gmail.com>
2025-10-15 11:03:10 +0200

    Initial configuration backend spring boot

---
+++ backend/src/main/java/com/mrr/treasury_tracker/TreasuryTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mrr.treasury_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasuryTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreasuryTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mrr/treasury_tracker/TreasuryTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrr.treasury_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreasuryTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
