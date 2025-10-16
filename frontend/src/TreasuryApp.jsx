35a2eb839dc5d22fd85cdde3cf27a102f9391a9f
MarcosRamaR <marcos.rama.1994@gmail.com>
2025-10-16 12:06:06 +0200

    add states

--- frontend/src/TreasuryApp.jsx
+++ frontend/src/TreasuryApp.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react"
 import { ExpenseForm } from "./components/ExpenseForm.jsx"
 
 export const TreasuryApp = () => {
+  const [expenses, setExpenses] = useState([])
+
+  const handleAddExpense = (expense) => {
+    setExpenses([...expenses, expense])
+    }
+  
+
   return (
     <>
       <div>
           <h1>Treasury Tracker</h1>
-          <ExpenseForm />
+          <ExpenseForm onSubmit={handleAddExpense}/>
       </div>
     </>
 
--- frontend/src/components/ExpenseForm.jsx
+++ frontend/src/components/ExpenseForm.jsx
@@ -1,15 +1,33 @@
 //Form to add or edit an expense
 
-export function ExpenseForm() {
+import { useState } from "react"
+
+export function ExpenseForm({onSubmit}) {
+
+    const [description, setDescription] = useState('')
+    const [amount, setAmount] = useState('')
+    const [date, setDate] = useState('')
+    const [category, setCategory] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(!description || !amount || !date || !category) return
+        onSubmit({description, amount: parseFloat(amount), date, category}) //Call parent function
+        //Clear form
+        setDescription('')
+        setAmount('')
+        setDate('')
+        setCategory('')
+    }
 
   return (
     <div>
         <h3>Add Expense</h3>
-        <form>
-            <input type="text" placeholder="Description" />
-            <input type="number" placeholder="Amount" />
-            <input type="date" placeholder="Date" />
-            <input type="text" placeholder="Category" />
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+            <input type="number" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
+            <input type="date" placeholder="Date" value={date} onChange={(e) => setDate(e.target.value)} />
+            <input type="text" placeholder="Category" value={category} onChange={(e) => setCategory(e.target.value)} />
             <button type="submit">Add Expense</button>
         </form>
     </div>
