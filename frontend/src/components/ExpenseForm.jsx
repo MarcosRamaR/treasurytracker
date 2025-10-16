ae463d096270dfd6a19f6b78f3e694648d10e4f7
MarcosRamaR <marcos.rama.1994@gmail.com>
2025-10-16 12:32:05 +0200

    refactor form to use object for expense, and change input category for select

--- frontend/src/components/ExpenseForm.jsx
+++ frontend/src/components/ExpenseForm.jsx
@@ -4,30 +4,36 @@ import { useState } from "react"
 
 export function ExpenseForm({onSubmit}) {
 
-    const [description, setDescription] = useState('')
-    const [amount, setAmount] = useState('')
-    const [date, setDate] = useState('')
-    const [category, setCategory] = useState('')
+    const [formData, setFormData] = useState({
+        description: '',
+        amount: '',
+        date: '',
+        category: ''
+    })
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if(!description || !amount || !date || !category) return
-        onSubmit({description, amount: parseFloat(amount), date, category}) //Call parent function
-        //Clear form
-        setDescription('')
-        setAmount('')
-        setDate('')
-        setCategory('')
+        if(!formData) return
+        onSubmit({...formData,
+            amount: parseFloat(formData.amount)}) //Call parent function
+        setFormData({description: '', amount: '', date: '', category: ''}) //Reset form
     }
+    const categories = ['Food', 'Transport', 'Entertainment ', 'Others']
 
   return (
     <div>
         <h3>Add Expense</h3>
         <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
-            <input type="number" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
-            <input type="date" placeholder="Date" value={date} onChange={(e) => setDate(e.target.value)} />
-            <input type="text" placeholder="Category" value={category} onChange={(e) => setCategory(e.target.value)} />
+            <input type="text" placeholder="Description" value={formData.description} onChange={(e) => setFormData({...formData, description: e.target.value})} />
+            <input type="number" placeholder="Amount" value={formData.amount} onChange={(e) => setFormData({...formData, amount: e.target.value})} />
+            <input type="date" placeholder="Date" value={formData.date} onChange={(e) => setFormData({...formData, date: e.target.value})} />
+            <select>
+                value={formData.category} onChange={(e) => setFormData({...formData, category: e.target.value})}
+                <option value="">Select Category</option>
+                {categories.map(cat => (
+                    <option key={cat} value={cat}>{cat}</option>
+                ))}
+            </select>
             <button type="submit">Add Expense</button>
         </form>
     </div>
