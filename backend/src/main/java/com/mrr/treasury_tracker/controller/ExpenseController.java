983a4bbef81b31c5627029042d93f0d64c82966d
MarcosRamaR <marcos.rama.1994@gmail.com>
2025-10-17 10:30:33 +0200

    changes to connect front and back

--- backend/src/main/java/com/mrr/treasury_tracker/controller/ExpenseController.java
+++ backend/src/main/java/com/mrr/treasury_tracker/controller/ExpenseController.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/expenses")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ExpenseController {
 
     @Autowired
--- frontend/src/TreasuryApp.jsx
+++ frontend/src/TreasuryApp.jsx
@@ -2,44 +2,42 @@ import { useState } from "react"
 import { ExpenseForm } from "./components/ExpenseForm.jsx"
 import { ExpenseList } from "./components/ExpenseList.jsx"
 import { ExpenseSummary } from "./components/ExpenseSummary.jsx"
+import { useExpenses } from "./hooks/useExpenses.js"
 
 export const TreasuryApp = () => {
-  const [expenses, setExpenses] = useState([])
+  const {
+    expenses,
+    loading,
+    error,
+    createExpense,
+    updateExpense,
+    deleteExpense
+  } = useExpenses()
+
   const [editExpense, setEditExpense] = useState(null)
 
-  const handleAddExpense = (expense) => {
-    setExpenses([...expenses, expense])
+  const handleAddExpense = async (expense) => {
+    await createExpense(expense)
     }
-  const handleDeleteExpense = (id) => {
-    setExpenses(expenses.filter(item => item.id !== id))
+  const handleDeleteExpense = async(id) => {
+    await deleteExpense(id)
   }
 
   const handleEditExpense = (expense) => {
     setEditExpense(expense)
   }
 
-const handleUpdateExpense = (expense) => {
+const handleUpdateExpense = async (expense) => {
   // Get the id of the expense being edited
   if (!expense) {
     setEditExpense(null)
     return
   }
-  const idExpense = editExpense.id
-  const updatedExpenses = expenses.map(item => {
-    if (item.id === idExpense) {
-      //Create a new object merging old and new data
-      const updatedData = {
-        ...item,          //The old data
-        ...expense     //The new data overrides the old one
-      }
-      return updatedData
-    }
-    return item
-  })
-  //Update the state with the new array
-  setExpenses(updatedExpenses)
+  await updateExpense(editExpense.id, expense)
   setEditExpense(null)
 }
+if (loading) return <div>Loading expenses...</div>
+if (error) return <div>Error: {error}</div>
 
 //For edit button (ExpenseList) call handleEditExpense, passing the expense to edit and set it in state (editExpense)
 // after that pass it to ExpenseForm as prop (editingExpense) and handleUpdateExpense as onSubmit 
