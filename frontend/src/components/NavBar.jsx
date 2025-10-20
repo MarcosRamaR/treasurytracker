f42efac33141ea2db1b657fb2a937367bad8e265
MarcosRamaR <marcos.rama.1994@gmail.com>
2025-10-20 12:55:15 +0200

    imported a NavBar from bootstrap

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,8 +2,25 @@
 
 export const NavBar = () => {
   return (
-    <nav>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a className="navbar-brand" href="#">Treasury Tracker</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Summary</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="#">Expenses</a>
+                </li>
 
+            </ul>
+            </div>
+        </div>
     </nav>
+
   )
 }
