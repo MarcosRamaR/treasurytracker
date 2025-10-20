0319e3d319a2287b0f2164833e880d5a61e6e3c3
MarcosRamaR <marcos.rama.1994@gmail.com>
2025-10-20 13:00:19 +0200

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,23 +1,22 @@
-
+import {NavLink} from 'react-router-dom';
 
 export const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a className="navbar-brand" href="#">Treasury Tracker</a>
+            <NavLink to='/' className="navbar-brand">Treasury Tracker</NavLink>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Summary</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Expenses</a>
-                </li>
-
-            </ul>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                    <NavLink to='/summary' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Summary</NavLink>
+                    </li>
+                    <li className="nav-item">
+                    <NavLink to='/expenses' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>Expenses</NavLink>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
