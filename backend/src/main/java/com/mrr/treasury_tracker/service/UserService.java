0439999c1d35e4cea513a36822035aaae7da2e97
MarcosRamaR <marcos.rama.1994@gmail.com>
2025-10-29 16:07:32 +0100

    add register method on service

--- backend/src/main/java/com/mrr/treasury_tracker/service/UserService.java
+++ backend/src/main/java/com/mrr/treasury_tracker/service/UserService.java
@@ -34,6 +34,14 @@ public class UserService implements UserDetailsService { //UserDetailsService is
         );
     }
 
-    
+    public User registerUser(User user){
+        //Avoid duplicated emails
+        if (userRepository.existsByEmail(user.getEmail())){
+            throw new RuntimeException("Email already exists");
+        }
+        //Encrypt password before save user
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
 
 }
