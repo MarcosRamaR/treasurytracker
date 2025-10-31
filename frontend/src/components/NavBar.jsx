d2127015d1ed0a45caf6037d4ed7ccfea167c19b
MarcosRamaR <marcos.rama.1994@gmail.com>
2025-10-31 14:32:11 +0100

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,16 +1,34 @@
 import {NavLink} from 'react-router-dom';
 import '../styles/NavBarStyle.css';
+import {authService} from '../services/authService';
 
 export const NavBar = () => {
-  return (
+    // Check if user is authenticated and user data
+    const isLogged = authService.isAuthenticated();
+    const user = authService.getUser();
+
+    //Functon to allow logout
+    const handleLogout = () => {
+        authService.logout();
+        window.location.href = '/'; // Redirect to login page after logout
+    }
+
+    return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top">
         <div className="container-fluid" >
             <NavLink to='/' className="navbar-brand">Treasury Tracker</NavLink>
+            {/*Show name if logged*/}
+            {isLogged && (
+                <span className="navbar-text me-3"> Welcome, {user.username}!</span>
+            )}
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
+
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    {!isLogged ? (
+                        <>
                     <li className="nav-item">
                         <NavLink to='/summary' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
                         Summary</NavLink>
@@ -23,6 +41,17 @@ export const NavBar = () => {
                         <NavLink to='/incomes' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}>
                         Incomes</NavLink>
                     </li>
+                    <li className="nav-item">
+                    <button 
+                        onClick={handleLogout} className="nav-link btn btn-link" >
+                        Logout
+                    </button>
+                    </li>
+                    </>
+                    ) : (<li className="nav-item">
+                        <NavLink to='/' className="nav-link" >Home</NavLink>
+                    </li>)}
+                    
                 </ul>
             </div>
         </div>
