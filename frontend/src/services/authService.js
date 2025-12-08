aed8a3da1214c06a531fa6da3202d6242f514eb0
MarcosRamaR <marcos.rama.1994@gmail.com>
2025-12-08 12:41:07 +0100

    Fixed registration to can show error message to user on page

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -11,8 +11,22 @@ export const authService = {
             },
             body: JSON.stringify(userData) //Object to JSON
         })
-        const data = await response.json()
+        const contentType = response.headers.get('content-type') //Verify response have content
+        let data
+
+        if(contentType && contentType.includes('application/json')) {
+            data = await response.json()
+        } else {
+            //If response void or not JSON, create custom error message
+            data = {
+                error: response.status === 403 
+                    ? 'User already exists' 
+                    : `Registration failed (${response.status})`
+            }
+        }
+        console.log('Register data:', data)
         return data
+        
     }catch (error) {
             console.error('Error during registration:', error)
             throw error
